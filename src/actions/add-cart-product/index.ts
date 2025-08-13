9da7e1956dd7de8aea025b6e15176049c8301f6e
ArthurDevWorks <arthurparente26@gmail.com>
2025-08-13 09:32:24 -0300

    feat: add product variant in cart

--- src/actions/add-cart-product/index.ts
+++ src/actions/add-cart-product/index.ts
@@ -9,25 +9,36 @@ import { auth } from "@/lib/auth";
 
 import { AddProductToCartSchema, addProductToCartSchema } from "./schema";
 
+//Adicionar produto no carrinho
 export const addProductToCart = async (data: AddProductToCartSchema) => {
   addProductToCartSchema.parse(data);
+
+  //Verifica se tem algum usuario logado
   const session = await auth.api.getSession({
     headers: await headers(),
   });
   if (!session?.user) {
     throw new Error("Unauthorized");
   }
+
+  //Busca o produto que vai ser inserido com base no id
   const productVariant = await db.query.productVariantTable.findFirst({
     where: (productVariant, { eq }) =>
       eq(productVariant.id, data.productVariantId),
   });
+
+  //Verifica se existe essa variant
   if (!productVariant) {
     throw new Error("Product variant not found");
   }
+
+  //Verifica se ja existe algum carrinho com esse user
   const cart = await db.query.cartTable.findFirst({
     where: (cart, { eq }) => eq(cart.userId, session.user.id),
   });
   let cartId = cart?.id;
+
+  //Se nao existir cria o carrinho
   if (!cartId) {
     const [newCart] = await db
       .insert(cartTable)
@@ -37,11 +48,15 @@ export const addProductToCart = async (data: AddProductToCartSchema) => {
       .returning();
     cartId = newCart.id;
   }
+
+  //Verica se ja existe esse produto no carrinho
   const cartItem = await db.query.cartItemTable.findFirst({
     where: (cartItem, { eq }) =>
       eq(cartItem.cartId, cartId) &&
       eq(cartItem.productVariantId, data.productVariantId),
   });
+
+  //Se existir aumenta a quantidade
   if (cartItem) {
     await db
       .update(cartItemTable)
@@ -51,6 +66,8 @@ export const addProductToCart = async (data: AddProductToCartSchema) => {
       .where(eq(cartItemTable.id, cartItem.id));
     return;
   }
+
+  //Se nao existir adiciona esse produto na tabela de carrinho
   await db.insert(cartItemTable).values({
     cartId,
     productVariantId: data.productVariantId,
--- src/actions/add-cart-product/schema.ts
+++ src/actions/add-cart-product/schema.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+//Faz a validação dos dados do carrinho antes de inserir no banco de dados
 export const addProductToCartSchema = z.object({
   productVariantId: z.uuid(),
   quantity: z.number().min(1),
